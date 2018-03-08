d90ec55a6fe53e4ede45bd95a26b1d8b72721498
cute apple <hidden>
2018-03-08 22:10:13 +0800

    ...
    just want to commit

--- autoencoder/autoencoder.py
+++ autoencoder/autoencoder.py
@@ -3,26 +3,30 @@ from keras.models import Model, Sequential
 from keras.datasets import mnist
 import numpy as np
 
-encoding_dim = 32
+# Captalize for Layer
 
-# this is our input placeholder
-input_img = Input(shape=(784,))
-# "encoded" is the encoded representation of the input
-encoded = Dense(encoding_dim, activation='relu')(input_img)
-# "decoded" is the lossy reconstruction of the input
-decoded = Dense(784, activation='sigmoid')(encoded)
 
-# this model maps an input to its reconstruction
-autoencoder = Model(input_img, decoded)
-encoder = Model(input_img, encoded)
+Encoder = Dense(32, activation='relu')
+Decoder = Dense(784, activation='sigmoid')
 
+x = Input(shape=(784,))
+z = Encoder(x)
+y = Decoder(z)
+
+autoencoder = Model(x, y)
+encoder = Model(x, z)
+
+_z = Input(shape=(32,))
+decoder = Model(_z, Decoder(_z))
+
+'''
 # create a placeholder for an encoded (32-dimensional) input
 encoded_input = Input(shape=(encoding_dim,))
 # retrieve the last layer of the autoencoder model
 decoder_layer = autoencoder.layers[-1]
 # create the decoder model
 decoder = Model(encoded_input, decoder_layer(encoded_input))
-
+'''
 autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
 
 (x_train, _), (x_test, _) = mnist.load_data()
@@ -34,7 +38,7 @@ print(x_train.shape)
 print(x_test.shape)
 
 autoencoder.fit(x_train, x_train,
-                epochs=20,
+                epochs=10,
                 batch_size=256,
                 shuffle=True,
                 validation_data=(x_test, x_test))
