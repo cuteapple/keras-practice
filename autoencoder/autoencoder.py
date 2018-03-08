46f53fd457d65e968bdde067146f805f9b526970
cute apple <hidden>
2018-03-08 21:50:48 +0800

    enable training

--- autoencoder/autoencoder.py
+++ autoencoder/autoencoder.py
@@ -1,5 +1,7 @@
 from keras.layers import Input, Dense
 from keras.models import Model, Sequential
+from keras.datasets import mnist
+import numpy as np
 
 encoding_dim = 32
 
@@ -23,8 +25,6 @@ decoder = Model(encoded_input, decoder_layer(encoded_input))
 
 autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
 
-from keras.datasets import mnist
-import numpy as np
 (x_train, _), (x_test, _) = mnist.load_data()
 x_train = x_train.astype('float32') / 255.
 x_test = x_test.astype('float32') / 255.
@@ -32,13 +32,13 @@ x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
 x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
 print(x_train.shape)
 print(x_test.shape)
-'''
+
 autoencoder.fit(x_train, x_train,
-                epochs=50,
+                epochs=20,
                 batch_size=256,
                 shuffle=True,
                 validation_data=(x_test, x_test))
-				'''
+				
 # encode and decode some digits
 # note that we take them from the *test* set
 encoded_imgs = encoder.predict(x_test)
