d5cec79fb358cbac31e76ee7571a2b4a11c826a2
cute apple <hidden>
2018-03-09 11:51:02 +0800

    use model
    use model to better flexibility
    retrain pretrained model error=0.1x

--- autoencoder/autoencoder.py
+++ autoencoder/autoencoder.py
@@ -10,8 +10,10 @@ del types
 
 options.load = True
 options.train = True
-options.epochs = 5
+options.epochs = 20
 options.save = True
+options.test = True
+options.plot_model = True
 options.filename = 'model.h5'
 
 #
@@ -34,25 +36,32 @@ print(x_test.shape) #(?,784)
 # Prepare Network
 #
 
-Encode = Dense(32, activation='relu',activity_regularizer=regularizers.l1(10e-5))
-Decode = Dense(784, activation='sigmoid')
+x = Input(shape=(784,))
+z = Dense(32, activation='relu',name='enc_d32')(x)
+Encode = Model(x,z,name='Encoder')
+
+z = Input(shape=(32,))
+y = Dense(784, activation='sigmoid',name='dec_d784')(z)
+Decode = Model(z,y,name='Decoder')
 
 x = Input(shape=(784,))
 z = Encode(x)
 y = Decode(z)
-z2 = Input(shape=(32,)) # wish I can use z directly o.o
-y2 = Decode(z2) # reuse layer (and it's weights)
 
-autoencoder = Model(x, y)
-encoder = Model(x, z)
-decoder = Model(z2, y2)
+autoencoder = Model(x,y)
+encoder = Encode
+decoder = Decode
 
-del x,z,y,z2,y2
+if options.plot_model:
+	from keras.utils import plot_model
+	plot_model(autoencoder, to_file='model.png',show_shapes=True)
+	plot_model(encoder, to_file='model_encoder.png',show_shapes=True)
+	plot_model(decoder, to_file='model_decoder.png',show_shapes=True)
 
-autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+
 
 #
-#
+# Start
 #
 
 if options.load:
@@ -63,6 +72,7 @@ if options.load:
 		print('load weights failed')
 
 if options.train:
+	autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
 	autoencoder.fit(
 		x_train, x_train,
 		epochs = options.epochs,
@@ -80,24 +90,25 @@ if options.save:
 # see result
 #
 
-# encode and decode test set
-encoded_imgs = encoder.predict(x_test)
-decoded_imgs = decoder.predict(encoded_imgs)
+if options.test:
+	# encode and decode test set
+	encoded_imgs = encoder.predict(x_test)
+	decoded_imgs = decoder.predict(encoded_imgs)
 
-import cv2
-import os
-import time
+	import cv2
+	import os
+	import time
 
-def to_img(z):
-	z = z*255
-	z = z.astype(int)
-	z = z.reshape((28,28,1))
-	return z
+	def to_img(z):
+		z = z*255
+		z = z.astype(int)
+		z = z.reshape((28,28,1))
+		return z
 
-outdir = 'o/{}'.format(int(time.time()))
-os.makedirs(outdir)
-outdir_for = lambda name,postfix='',extension='png': '{}/{}_{}.{}'.format(outdir,name,postfix,extension)
+	outdir = 'o/{}'.format(int(time.time()))
+	os.makedirs(outdir)
+	outdir_for = lambda name,postfix='',extension='png': '{}/{}_{}.{}'.format(outdir,name,postfix,extension)
 
-for i in range(10):
-    cv2.imwrite(outdir_for(i,'i'),to_img(x_test[i]))
-    cv2.imwrite(outdir_for(i,'o'),to_img(decoded_imgs[i]))
+	for i in range(10):
+		cv2.imwrite(outdir_for(i,'i'),to_img(x_test[i]))
+		cv2.imwrite(outdir_for(i,'o'),to_img(decoded_imgs[i]))
